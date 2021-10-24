234d283864b495445182bcbedc427ea65eb8ed9c
Norbert <n0rb3r7@outlook.com>
2021-10-24 11:48:47 -0300

    Check email if already exists.

--- services/saveEmail.php
+++ services/saveEmail.php
@@ -1,29 +1,37 @@
 <?php
-$error = "";
 $servername = "localhost";
 $database = "emailslanding";
 $username = "root";
 $password = "";
 $sql = "mysql:host=$servername;dbname=$database;";
 $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$error = "";
 try {
-    $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
+    $db_connection = new PDO($sql, $username, $password, $dsn_Options);
 } catch (PDOException $error) {
     echo 'Connection error: ' . $error->getMessage();
 }
-$email = $_POST['thisemail'];
 
 if (empty($_POST['thisemail'])) {
     $error = "Ingrese su email";
 } else {
+    if (!filter_var($_POST['thisemail'], FILTER_VALIDATE_EMAIL)) {
+        $error = "Email incorrecto";
+    }
     $email = $_POST['thisemail'];
 }
-$saveEmail = $my_Db_Connection->prepare("INSERT INTO useremails (email) VALUES (:email)");
-$saveEmail->bindValue(':email', $email);
-
-if (empty($error)) {
+$checkEmail = $db_connection->prepare("SELECT * FROM useremails WHERE email=:email");
+$checkEmail->bindParam(':email', $email);
+$checkEmail->execute();
+if ($checkEmail->rowCount() != 0) {
+    $error = "Email ya registrado, intente con otro.";
+} else {
+    $saveEmail = $db_connection->prepare("INSERT INTO useremails (email) VALUES (:email)");
+    $saveEmail->bindValue(':email', $email);
     $saveEmail->execute();
-    $output = json_encode(array('type' => 'message', 'text' => "email agregado"));
+}
+if (empty($error)) {
+    $output = json_encode(array('type' => 'message', 'text' => "¡Muchas gracias! Hemos registrado su email."));
     die($output);
 } else {
     $output = json_encode(array('type' => 'error', 'text' =>  $error));
